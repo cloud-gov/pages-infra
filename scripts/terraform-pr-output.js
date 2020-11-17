b653da0f92b86ac3e91e62c7ebe256b60d16a898
David Corwin <david.corwin@gsa.gov>
2020-11-17 09:54:10 -0800

    add sript

---
+++ scripts/terraform-pr-output.js
@@ -0,0 +1,29 @@
+
+function displayOutcome(outcome) {
+  return outcome === 'success' ? ✅ : ❌ ;
+}
+
+module.exports = (github, ctx) => {
+  const { PLAN } = process.env;
+  const { issue, matrix, repo, steps } = ctx;
+    
+  const output = `
+    ## Terraforming *${matrix.name}*
+    #### Format: ${displayOutcome(steps.format.outcome)}
+    #### Init:   ${displayOutcome(steps.init.outcome)}
+    #### Plan:   ${displayOutcome(steps.plan.outcome)}
+    <details>
+      <summary>
+        Show Plan
+      </summary>
+      ${PLAN}
+    </details>
+  `;
+
+  github.issues.createComment({
+    issue_number: issue.number,
+    owner: repo.owner,
+    repo: repo.repo,
+    body: output
+  });
+}
